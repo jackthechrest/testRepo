038addc175afc051598d03e75049c285b0f4edf3
jackthechrest <120044450+jackthechrest@users.noreply.github.com>
2023-04-16 15:38:24 -0500

    Update utils.d.ts

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -3,3 +3,40 @@ type DatabaseConstraintError = {
   columnName?: string;
   message?: string;
 };
+
+type NewUserRequest = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+type UserIdParam = {
+  targetUserId: string;
+};
+
+type NewEmailBody = {
+  email: string;
+};
+
+type AuthRequest = {
+  email: string;
+  password: string;
+};
+
+// WIP: Store Question IDs here, use short names that line up with what question asks
+type QuestionId = 'PlaceholderID1' | 'PlaceholderID2';
+
+type QuestionData = Partial<Record<QuestionId, string>>;
+
+type RulesOfLoveOptions = 'Rock Candy Heart' | 'Box of Chocolates' | 'Candle' | 'NONE';
+
+type RulesOfLoveBody = {
+  gameId: string;
+  newPlay: RulesOfLoveOptions;
+};
+
+// color options for Copycat (in order): white (blank), red, yellow, blue, orange, purple, green
+type ColorOptions = 'w' | 'r' | 'y' | 'b' | 'o' | 'p' | 'g';
+
+// roles in Copycat
+type CopycatRoles = 'creator' | 'recreator';
